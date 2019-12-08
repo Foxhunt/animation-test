f2ae3e7d33cb6d054af7b80e6332d9a7b258c351
Marco <marco@mojica.de>
2019-12-08 18:57:12 +0100

    Add some animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,64 @@
 import React from 'react'
 import styled from 'styled-components'
+import { motion, Variants } from "framer-motion"
 
-const App = styled.div`
-  width: 100vw;
-  height: 100vh
+const Svg = styled(motion.svg)`
+  width: 100px;
+  height: 100px;
+  
+  overflow: visible;
+  stroke-width: 2;
+  stroke-linejoin: round;
+  stroke-linecap: round;
 `
 
-export default () => <App>My page</App>
+const svg: Variants = {
+  hidden: {
+    rotate: "0deg",
+  },
+  visible: {
+    rotate: "0deg",
+    transition: {
+      duration: 1,
+      delayChildren: 0.25
+    }
+  }
+}
+
+const paths: Variants = {
+  hidden: (i: number) => ({
+    stroke: "#fff",
+    pathLength: 0,
+    fill: "rgba(255, 255, 255, 0)",
+    rotate: "-45deg",
+    y: 0 * i
+  }),
+  visible: (i: number) => ({
+    stroke: "#000",
+    pathLength: 1,
+    fill: "rgba(140, 255, 255, 1)",
+    rotate: "0deg",
+    y: 15 * i,
+    transition: {
+      default: { duration: 0.2 },
+      y: { delay: 0.5, duration: 0.3 }
+    }
+  })
+}
+
+
+export default () => <Svg
+  variants={svg}
+  initial={"hidden"}
+  animate={"visible"}>
+  <motion.path
+    custom={1}
+    d="M50 85L15 50L50 15"
+    variants={paths}
+  />
+  <motion.path
+    custom={-1}
+    d="M50 15L85 50L50 85"
+    variants={paths}
+  />
+</Svg>
