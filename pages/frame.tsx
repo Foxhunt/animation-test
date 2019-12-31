c6ad0de731b3ef9c6e3c7d5d82e2b1dc3bbadcbe
Marco <marco@mojica.de>
2019-12-31 18:11:16 +0100

    Add another defensive layer

--- pages/frame.tsx
+++ pages/frame.tsx
@@ -15,7 +15,7 @@ const circleTB: Variants = {
         }
     },
     expanded: (i: number) => ({
-        r: [2, 0],
+        r: [2, 1],
         y: [1 * i, 2 * i],
         transition: {
             default: {
@@ -32,7 +32,7 @@ const frameGroup: Variants = {
     expanded: {
         transition: {
             delayChildren: 5,
-            staggerChildren: .4
+            staggerChildren: .9
         }
     }
 }
@@ -40,13 +40,19 @@ const frameGroup: Variants = {
 const frameLine: Variants = {
     hidden: {
         stroke: "#fff",
+        fill: "#fff",
         pathLength: 0,
         opacity: 0
     },
     collapsed: {
         stroke: "#fff",
         pathLength: 0,
-        opacity: 0
+        opacity: 0,
+        transition: {
+            default: {
+                duration: 0.5
+            }
+        }
     },
     expanded: {
         stroke: "#000",
@@ -57,7 +63,8 @@ const frameLine: Variants = {
                 duration: 0
             },
             pathLength: {
-                duration: .5
+                duration: 1,
+                ease: "easeOut"
             }
         }
     }
@@ -84,7 +91,8 @@ const frameDots: Variants = {
         y,
         transition: {
             default: {
-                duration: .5
+                duration: 1,
+                ease: "easeOut"
             },
             r: {
                 duration: 0
@@ -120,6 +128,50 @@ export default function Frame() {
             <motion.path
                 variants={frameLine}
                 d="M 14 29L -7 50" />
+            <motion.circle
+                variants={frameDots}
+                cx={-7}
+                cy={50}
+                custom={{
+                    x: 0,
+                    y: -14
+                }} />
+            <motion.path
+                variants={frameLine}
+                d="M -7 36L -7 22" />
+            <motion.circle
+                variants={frameDots}
+                cx={-7}
+                cy={22}
+                custom={{
+                    x: 0,
+                    y: -14
+                }} />
+            <motion.path
+                variants={frameLine}
+                d="M -7 8L -7 -7L 8 -7" />
+            <motion.circle
+                variants={frameDots}
+                cx={8}
+                cy={-7}
+                custom={{
+                    x: 14,
+                    y: 0
+                }} />
+            <motion.path
+                variants={frameLine}
+                d="M 22 -7L 36 -7" />
+            <motion.circle
+                variants={frameDots}
+                cx={36}
+                cy={-7}
+                custom={{
+                    x: -14,
+                    y: 14
+                }} />
+            <motion.path
+                variants={frameLine}
+                d="M 22 7l -14 0l 0 14" />
         </motion.g>
 
         {/* top right */}
@@ -147,6 +199,50 @@ export default function Frame() {
             <motion.path
                 variants={frameLine}
                 d="M 86 29L 107 50" />
+            <motion.circle
+                variants={frameDots}
+                cx={50}
+                cy={-7}
+                custom={{
+                    x: 14,
+                    y: 0
+                }} />
+            <motion.path
+                variants={frameLine}
+                d="M 64 -7L 78 -7" />
+            <motion.circle
+                variants={frameDots}
+                cx={78}
+                cy={-7}
+                custom={{
+                    x: 14,
+                    y: 0
+                }} />
+            <motion.path
+                variants={frameLine}
+                d="M 92 -7L 107 -7L 107 8" />
+            <motion.circle
+                variants={frameDots}
+                cx={107}
+                cy={8}
+                custom={{
+                    x: 0,
+                    y: 14
+                }} />
+            <motion.path
+                variants={frameLine}
+                d="M 107 22L 107 36" />
+            <motion.circle
+                variants={frameDots}
+                cx={107}
+                cy={36}
+                custom={{
+                    x: -14,
+                    y: -14
+                }} />
+            <motion.path
+                variants={frameLine}
+                d="M 93 22l 0 -14l -14 0" />
         </motion.g>
 
         {/* bottom left */}
@@ -174,6 +270,50 @@ export default function Frame() {
             <motion.path
                 variants={frameLine}
                 d="M 14 71L -7 50" />
+            <motion.circle
+                variants={frameDots}
+                cx={50}
+                cy={107}
+                custom={{
+                    x: -14,
+                    y: 0
+                }} />
+            <motion.path
+                variants={frameLine}
+                d="M 36 107L 22 107" />
+            <motion.circle
+                variants={frameDots}
+                cx={22}
+                cy={107}
+                custom={{
+                    x: -14,
+                    y: 0
+                }} />
+            <motion.path
+                variants={frameLine}
+                d="M 8 107L -7 107L -7 92" />
+            <motion.circle
+                variants={frameDots}
+                cx={-7}
+                cy={92}
+                custom={{
+                    x: 0,
+                    y: -14
+                }} />
+            <motion.path
+                variants={frameLine}
+                d="M -7 78L -7 64" />
+            <motion.circle
+                variants={frameDots}
+                cx={-7}
+                cy={64}
+                custom={{
+                    x: 14,
+                    y: 14
+                }} />
+            <motion.path
+                variants={frameLine}
+                d="M 7 78l 0 14l 14 0" />
         </motion.g>
 
         {/* bottom right*/}
@@ -201,6 +341,50 @@ export default function Frame() {
             <motion.path
                 variants={frameLine}
                 d="M 86 71L 107 50" />
+            <motion.circle
+                variants={frameDots}
+                cx={107}
+                cy={50}
+                custom={{
+                    x: 0,
+                    y: 14
+                }} />
+            <motion.path
+                variants={frameLine}
+                d="M 107 64L 107 78" />
+            <motion.circle
+                variants={frameDots}
+                cx={107}
+                cy={78}
+                custom={{
+                    x: 0,
+                    y: 14
+                }} />
+            <motion.path
+                variants={frameLine}
+                d="M 107 92L 107 107L 92 107" />
+            <motion.circle
+                variants={frameDots}
+                cx={92}
+                cy={107}
+                custom={{
+                    x: -14,
+                    y: 0
+                }} />
+            <motion.path
+                variants={frameLine}
+                d="M 78 107L 64 107" />
+            <motion.circle
+                variants={frameDots}
+                cx={64}
+                cy={107}
+                custom={{
+                    x: 14,
+                    y: -14
+                }} />
+            <motion.path
+                variants={frameLine}
+                d="M 78 93l 14 0l 0 -14" />
         </motion.g>
 
         <motion.circle
